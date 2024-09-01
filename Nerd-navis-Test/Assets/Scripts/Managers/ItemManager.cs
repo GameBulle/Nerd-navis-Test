6953697eaeef9c778ce151a5792606701b2573f0
Constellation6 <160034276+GameBulle@users.noreply.github.com>
2024-09-01 21:26:18 +0900

    ItemManager 수정 및 멤버 변수 추가
    1. List<Vector2> l_LimitAndCostList, List<int>[] l_UpgradeValueList 멤버 변수를 추가하여 ItemOptionUpgrade.CSV를 읽어서 l_LimitAndCostList에 Vector.x = n_UpgradeBelowLimit, Vector.y = n_UpgradeCost 값을 저장하고, l_UpgradeValueList에는 item의 normal, rare, epic 등급에 따른 능력치 증가량을 저장함

--- Nerd-navis-Test/Assets/Scripts/Managers/ItemManager.cs
+++ Nerd-navis-Test/Assets/Scripts/Managers/ItemManager.cs
@@ -1,7 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System;  
+using System;
+using System.Runtime.InteropServices;
 
 public class ItemManager : MonoBehaviour
 {
@@ -10,10 +11,10 @@ public class ItemManager : MonoBehaviour
     {
         get
         {
-            if(instance == null)
+            if (instance == null)
             {
                 instance = FindObjectOfType<ItemManager>();
-                if(!instance)
+                if (!instance)
                     instance = new GameObject("ItemManager").AddComponent<ItemManager>();
             }
             return instance;
@@ -24,6 +25,8 @@ public class ItemManager : MonoBehaviour
     public enum ItemOptionType { AttackIncrease, DefenseIncrease, HpIncrease }
 
     List<Item> l_ItemList;
+    List<Vector2> l_LimitAndCostList;   // x : 강화 단계 이하 조건, y : 단계 별 요구 비용
+    List<int>[] l_UpgradeValueList;
 
     private void Awake()
     {
@@ -31,14 +34,15 @@ public class ItemManager : MonoBehaviour
             Destroy(gameObject);
 
         l_ItemList = new();
+        l_LimitAndCostList = new();
     }
 
     public void Initialize()
     {
         List<Dictionary<string, string>> l_Data = CSV.Read("ItemList");
-        for(int i=1;i<l_Data.Count;i++)
+        for (int i = 1; i < l_Data.Count; i++)
         {
-            Item i_Item =  gameObject.AddComponent<Item>();
+            Item i_Item = gameObject.AddComponent<Item>();
             i_Item.Initialize(
                 int.Parse(l_Data[i]["n_ItemID"]),
                 (ItemGrade)Enum.Parse(typeof(ItemGrade), l_Data[i]["e_ItemGrade"]),
@@ -46,6 +50,22 @@ public class ItemManager : MonoBehaviour
                 int.Parse(l_Data[i]["n_DefaultValue"]),
                 l_Data[i]["s_IconPath"]);
             l_ItemList.Add(i_Item);
+        }
+
+        List<Dictionary<string, string>> l_Data2 = CSV.Read("ItemOptionUpgrade");
+        l_UpgradeValueList = new List<int>[l_Data2.Count - 1];
+        for (int i = 0; i < l_Data2.Count - 1; i++)
+            l_UpgradeValueList[i] = new();
+
+        for (int i = 1; i < l_Data2.Count; i++)
+        {
+            Vector2 v;
+            v.x = int.Parse(l_Data2[i]["n_UpgradeBelowLimit"]);
+            v.y = int.Parse(l_Data2[i]["n_UpgradeCost"]);
+            l_LimitAndCostList.Add(v);
+            l_UpgradeValueList[(int)ItemManager.ItemGrade.Normal].Add(int.Parse(l_Data2[i]["n_NormalUpgradeValue"]));
+            l_UpgradeValueList[(int)ItemManager.ItemGrade.Rare].Add(int.Parse(l_Data2[i]["n_RareUpgradeValue"]));
+            l_UpgradeValueList[(int)ItemManager.ItemGrade.Epic].Add(int.Parse(l_Data2[i]["n_EpicUpgradeValue"]));
         }
     }
 }
