26b7ea6bb621d2bc72b4ea096750814bd8b1d3e4
Constellation6 <160034276+GameBulle@users.noreply.github.com>
2024-09-03 21:51:20 +0900

    ResourceMenu Class 예외 처리 및 주석 작성
    1. 자원 획득 시, 소유한 자원량이 소유 가능한 자원 한계치보다 많을 때 획득 가능 알림판의 활성화 유무 수정
    
    2. 변수 및 특정 함수에 주석 작성

--- Nerd-navis-Test/Assets/Scripts/ResourceMenu.cs
+++ Nerd-navis-Test/Assets/Scripts/ResourceMenu.cs
@@ -11,8 +11,8 @@ public class ResourceMenu : MonoBehaviour
     [SerializeField] GameObject CanGetNoticeBoard;
     [SerializeField] TextMeshProUGUI MakedMoneyText;
 
-    float f_MaxMoneyLimit;
-    float f_CanGetRefillMoneyCount;
+    float f_MaxMoneyLimit;          // 얻을 수 있는 최대 자원량
+    float f_CanGetRefillMoneyCount; // 획득 가능한 자원의 최소양
 
     private void FixedUpdate()
     {
@@ -49,13 +49,16 @@ public class ResourceMenu : MonoBehaviour
 
     public void OnClickGetMoneyButton()
     {
-        if (InterfaceManager.Instance.MakedMoney < f_CanGetRefillMoneyCount 
-            || !CanGetNoticeBoard.activeSelf 
-            || GameManager.Instance.Money >= f_MaxMoneyLimit)
+        if (InterfaceManager.Instance.MakedMoney < f_CanGetRefillMoneyCount     // 생산된 자원량이 획득 가능한 최소 자원량보다 작은지
+            || !CanGetNoticeBoard.activeSelf                                    // 획득 가능 알림판이 비활성화 됐는지
+            || GameManager.Instance.Money >= f_MaxMoneyLimit)                   // 소유한 자원이 소유 가능한 자원 한계치보다 많은지
             return;
 
+        // 소유 자원 + 생산된 자원이 소유 가능한 자원 한계치보다 많을 때
         if (GameManager.Instance.Money + InterfaceManager.Instance.MakedMoney >= f_MaxMoneyLimit)
         {
+            // 소유한 자원양을 소유 가능한 자원 한계치까지 증가시키고
+            // 증가시키고 남은 자원량을 현재 생산된 자원량에서 뺌
             float n_Left = f_MaxMoneyLimit - GameManager.Instance.Money;
             GameManager.Instance.Money += (int)n_Left;
             InterfaceManager.Instance.MakedMoney -= n_Left;
@@ -66,8 +69,12 @@ public class ResourceMenu : MonoBehaviour
             InterfaceManager.Instance.MakedMoney = 0f;
         }
 
-        CanGetNoticeBoard.SetActive(false);
-        MakedMoneyText.color = Color.red;
+        if(InterfaceManager.Instance.MakedMoney < f_CanGetRefillMoneyCount) // 획득하고 남은 자원량이 획득 가능한 자원의 최소양 보다 적은지
+        {
+            CanGetNoticeBoard.SetActive(false);
+            MakedMoneyText.color = Color.red;
+        }
+
         MakedMoneyText.text = GameManager.Instance.MakeValueUnit(InterfaceManager.Instance.MakedMoney);
     }
 }
