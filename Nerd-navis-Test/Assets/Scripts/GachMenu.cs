de4bf9c583b4e952c5cb5c0abf7d907132d256cc
Constellation6 <160034276+GameBulle@users.noreply.github.com>
2024-09-03 22:04:15 +0900

    GachMenu Class 수정 및 주석 작성
    1. Initialize 함수에서 무기탭 UI가 먼저 포커싱 되도록 수정
    
    2. OnClickGachButton(int GachTime)에서 GachTime -> GachaTime 오타 수정
    
    3. 변수 및 특정 함수에 주석 작성

--- Nerd-navis-Test/Assets/Scripts/GachMenu.cs
+++ Nerd-navis-Test/Assets/Scripts/GachMenu.cs
@@ -6,37 +6,41 @@ using UnityEngine;
 public class GachMenu : MonoBehaviour
 {
     [SerializeField] GachaResult GachaResult;
-    [SerializeField] Inventory[] inventories;
+    [SerializeField] Inventory[] inventories;   // Weapon, Defence, Shield
 
-    float f_RequireGachaPrice;
-    int GachaID;
+    float f_RequireGachaPrice;  // 뽑기 1회에 필요한 자원량
+    int n_GachaID;  // 현재 장비탭에 따른 GachaID
 
     public void Initialize(float gachaPrice)
     {
         f_RequireGachaPrice = gachaPrice;
-        //Array.Sort(inventories, (x, y) => (int)x.ItemOption < (int)y.ItemOption ? 1 : -1);
-        inventories[0].gameObject.SetActive(true);
-        GachaID = 10000;
+        n_GachaID = 10000;
         for (int i = 0; i < inventories.Length; i++)
+        {
+            if (i == 0)     // 뽑기 메뉴 활성화 시, 무기 UI가 먼저 포커싱 되도록 함
+                inventories[i].gameObject.SetActive(true);
+            else
+                inventories[i].gameObject.SetActive(false);
             inventories[i].Initialize();
+        }
         GachaResult.Initialize();
     }
 
-    public void OnClickGachaButton(int GachTime)
+    public void OnClickGachaButton(int GachaTime)
     {
-        if (f_RequireGachaPrice * GachTime > GameManager.Instance.Money)
+        if (f_RequireGachaPrice * GachaTime > GameManager.Instance.Money)    // 시행할려는 뽑기 횟수만큼 자원이 있는지 확인
             return;
             
-        GameManager.Instance.Money -= (int)f_RequireGachaPrice * GachTime;
-        Gacha gacha = GachaManager.Instance.GetGacha(GachaID);
+        GameManager.Instance.Money -= (int)f_RequireGachaPrice * GachaTime;
+        Gacha gacha = GachaManager.Instance.GetGacha(n_GachaID);    // 현재 활성화된 장비 UI(Inventory)에 맞는 뽑기 정보를 가져옴
 
-        Dictionary<int, int> d_PickedItemDic = gacha.PickRandomItem(GachTime);
+        Dictionary<int, int> d_PickedItemDic = gacha.PickRandomItem(GachaTime); // Key : 뽑기로 얻은 아이템의 ID, Value : 뽑기로 얻은 아이템의 수량
         foreach (KeyValuePair<int, int> d in d_PickedItemDic)
         {
             ItemManager.Instance.CheckIncreaseItem(d.Key, d.Value);
             for(int i=0;i<inventories.Length;i++)
             {
-                if (inventories[i].Type == ItemManager.Instance.GetItem(d.Key).Type)
+                if (inventories[i].Type == ItemManager.Instance.GetItem(d.Key).Type)    // 뽑은 Item의 옵션 타입에 맞는 Inventory로 얻은 Item 전달
                 {
                     inventories[i].SetItem(d.Key);
                     break;
@@ -44,15 +48,15 @@ public class GachMenu : MonoBehaviour
             }
         }
 
-        GachaResult.GachaProduction(d_PickedItemDic);
+        GachaResult.GachaProduction(d_PickedItemDic);   // 뽑기 연출 실행
 
         for (int i = 0; i < inventories.Length; i++)
-            inventories[i].UpdateInventoryUI();
+            inventories[i].UpdateInventoryUI();     // 장비 UI 최신화
     }
 
-    public void OnClickWeaponTab()
+    public void OnClickWeaponTab()  // 무기탭 클릭 했을 때
     {
-        GachaID = 10000;
+        n_GachaID = 10000;
         for (int i = 0; i < inventories.Length; i++)
         {
             if (inventories[i].Type == ItemManager.ItemOptionType.AttackIncrease)
@@ -62,9 +66,9 @@ public class GachMenu : MonoBehaviour
         }
     }
 
-    public void OnClickArmorTab()
+    public void OnClickArmorTab()   // 방어구탭 클릭 했을 때
     {
-        GachaID = 20000;
+        n_GachaID = 20000;
         for (int i = 0; i < inventories.Length; i++)
         {
             if (inventories[i].Type == ItemManager.ItemOptionType.DefenseIncrease)
@@ -74,9 +78,9 @@ public class GachMenu : MonoBehaviour
         }
     }
 
-    public void OnClickShieldTab()
+    public void OnClickShieldTab()  // 방패탭 클릭 했을 때
     {
-        GachaID = 30000;
+        n_GachaID = 30000;
         for (int i = 0; i < inventories.Length; i++)
         {
             if (inventories[i].Type == ItemManager.ItemOptionType.HpIncrease)
