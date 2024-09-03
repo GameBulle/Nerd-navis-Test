d994de8c9dee67418722cdb6b09df8a2ecf6d60d
Constellation6 <160034276+GameBulle@users.noreply.github.com>
2024-09-03 22:29:39 +0900

    ItemInfoPopUp Class 수정 및 주석 작성
    1. Slot -> InfoPopUpSlotPrefab 변수명 수정
    
    2. 특정 함수에 주석 작성

--- Nerd-navis-Test/Assets/Scripts/ItemInfoPopUp.cs
+++ Nerd-navis-Test/Assets/Scripts/ItemInfoPopUp.cs
@@ -11,21 +11,21 @@ public class ItemInfoPopUp : MonoBehaviour
     [SerializeField] Transform SlotTransform;
     [SerializeField] GameObject PopUpObject;
 
-    InventoryItemSlot Slot;
+    InventoryItemSlot InfoPopUpSlotPrefab;
 
     private void Awake()
     {
-        Slot = Instantiate(Resources.Load<InventoryItemSlot>("Prefab/InventoryItemSlot"), SlotTransform);
+        InfoPopUpSlotPrefab = Instantiate(Resources.Load<InventoryItemSlot>("Prefab/InventoryItemSlot"), SlotTransform);
     }
 
     public void SetItemInfoSlot(int ItemID)
     {
-        PopUpObject.gameObject.SetActive(true);
-        Slot.SetInventoryItemSlot(ItemID);
+        PopUpObject.gameObject.SetActive(true); // 팝업 활성화
+        InfoPopUpSlotPrefab.SetInventoryItemSlot(ItemID);   // Slot에 아이템 정보 셋팅
         Item item = ItemManager.Instance.GetItem(ItemID);
-        ValueText.text = GameManager.Instance.MakeValueUnit(item.Value);
+        ValueText.text = GameManager.Instance.MakeValueUnit(item.Value);    // 아이템의 능력치
 
-        switch(item.Type)
+        switch(item.Type)   // 아이템의 등급에 따른 Icon 가져오기
         {
             case ItemManager.ItemOptionType.AttackIncrease:
                 TypeIcon.sprite = Resources.Load<Sprite>("Image/Ui/Attack");
